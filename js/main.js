3cfb52dd318bf59d8c65f88ec60bde58f90f73d4
adeptnova <niraj.panchal@adeptnova.com>
2024-05-26 23:13:19 +0530

    V1 -- UI FIXES

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
-   
+    var ai = document.getElementById('home-ai');
+    var typewriter = new Typewriter(ai, {
+        loop: true,
+        delay: 75,
+      });
+
+      typewriter
+      .typeString('Artificial Intelligence')
+      .pauseFor(1000) 
+      .deleteChars(24)
+      .typeString("Automation")
+      .pauseFor(1000) 
+      .deleteChars(11)
+      .typeString("Data Modernization")
+      .pauseFor(1000) 
+      .deleteChars(20)
+      .start();
 });
 
 function openTab(btnName, seviceid){
